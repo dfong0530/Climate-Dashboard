d82b3e0b5eceabaa85a21607a4e4ced7f8d6ce0d
David Fong <dfong633@gmail.com>
2022-05-28 10:57:04 -0400

    warnings

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
 import "../CSS/Filter.css";
 
